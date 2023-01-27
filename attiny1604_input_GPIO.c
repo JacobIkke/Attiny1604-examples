6db610abb52538bdfa7e63339153ccca34bf9f66
PeetGaming <110664522+JacobIkke@users.noreply.github.com>
2023-01-27 16:44:25 +0100

    Update attiny1604_input_GPIO.c

--- attiny1604_input_GPIO.c
+++ attiny1604_input_GPIO.c
@@ -15,10 +15,11 @@ void gpio_init(void)
 	// PORTA bit 0-7
 	PORTA.OUT  |= PIN0_bm; //PA0 as input
 	PORTA.DIR &= ~ PIN0_bm;
-	PORTA.PIN0CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc; // Set pull up, and trigger on raising and falling edge
+	PORTA.PIN0CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc; // Set pull up, and trigger on raising and falling edge
+
 	// PORTB bit 0-3
-    PORTB.OUT |= PIN2_bm; // PB2 as output
-    PORTB.DIR |= PIN2_bm;
+    	PORTB.OUT |= PIN2_bm; // PB2 as output
+    	PORTB.DIR |= PIN2_bm;
 }
 
 void toggle_led(void)
@@ -30,13 +31,13 @@ int main(void)
 {
 	gpio_init();
 	
-    while (1) 
-    {
+   	 while (1) 
+    	{
 		if(~PORTA.IN & PIN0_bm) // check PORTA for changes, bitmask pin0 so we get only that bit.
 		{
 			toggle_led();
 			_delay_ms(200);
 		}	
-    }
+    	}
 }
 
