861254e536844e95e22712c00076ec54647ec965
PeetGaming <110664522+JacobIkke@users.noreply.github.com>
2024-01-19 21:39:42 +0100

    Update Attiny1604_input_GPIO.c

--- Attiny1604_input_GPIO.c
+++ Attiny1604_input_GPIO.c
@@ -13,8 +13,7 @@
 void gpio_init(void)
 {
 	// PORTA bit 0-7
-	PORTA.OUT  |= PIN1_bm; //PA1 as input
-	PORTA.DIR &= ~ PIN1_bm;
+	PORTA.DIR &= ~ PIN1_bm; // Set DIR as input(0)
 	PORTA.PIN1CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc; // Set pull up, and trigger on raising and falling edge
 
 	// PORTB bit 0-3
